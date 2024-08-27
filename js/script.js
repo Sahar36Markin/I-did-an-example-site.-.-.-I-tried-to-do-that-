557893bb1e9458dbf7b5567102110fe682ea2d9d
sahar_markin <130451533+Sahar36Markin@users.noreply.github.com>
2024-08-27 13:38:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//* ==================== typing animation ==================== *//
+var typed = new Typed('.typing',{
+    strings:["", "Web designer", "web developer", "Graphic designer", "Install websites", "and more..."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
